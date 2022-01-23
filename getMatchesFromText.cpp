57bfeeff479df3c2dfb17cd28e5fc9e3d78c82b2
Andrey Zhizhelev <46599232+andzh1@users.noreply.github.com>
2022-01-23 21:28:34 +0300

    Add files via upload

---
+++ getMatchesFromText.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main() {
+    freopen("matches.txt", "w", stdout);
+    freopen("data.txt", "r", stdin);
+
+    std::map<std::string, int> matches; // for each pair of letters (such as "fg", "aa", etc.) we calculate number of it's matches in the text
+
+    
+}
